67a8f09c2bb19246f42db82a46d91e5d03b187ae
Jim Kang <jimkang@gmail.com>
2015-01-14 20:47:47 -0500

    Whitespace.

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ function isTweetOfUser(username, tweet) {
 }
 
 function isRetweetOfUser(username, tweet) {
-    return (
-      (
-        tweet.retweeted_status && tweet.retweeted_status.user && 
-        tweet.retweeted_status.user.screen_name === username
-      )
-      ||
-      tweet.text.indexOf('RT @' + username) !== -1 ||
-      tweet.text.indexOf('"@' + username) !== -1 ||
-      tweet.text.indexOf(fancyLeftQuote + '@' + username) !== -1
-    );
+  return (
+    (
+      tweet.retweeted_status && tweet.retweeted_status.user && 
+      tweet.retweeted_status.user.screen_name === username
+    )
+    ||
+    tweet.text.indexOf('RT @' + username) !== -1 ||
+    tweet.text.indexOf('"@' + username) !== -1 ||
+    tweet.text.indexOf(fancyLeftQuote + '@' + username) !== -1
+  );
 }
+
 
 module.exports = {
   isTweetOfUser: isTweetOfUser,
